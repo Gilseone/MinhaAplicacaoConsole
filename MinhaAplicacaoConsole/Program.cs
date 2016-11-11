62588ad0691e8f3c17d86e0277108fa4fc77e370
gilse <gilse@DESKTOP-67FGMVM>
2016-11-11 19:18:17 -0200

    Add project files.

---
+++ MinhaAplicacaoConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaAplicacaoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de uma variável do tipo String
+            string strNome;
+            //Mostrando uma mensagem na tela
+            Console.Write("Informe seu nome: ");
+            //Fazendo uma leitura do teclado
+            strNome = Console.ReadLine();
+            //Mostrando uma mensagem na tela, utilizando o que foi lido do teclado
+            Console.WriteLine("Olá " + strNome + ". Seja bem vind@!");
+            //Mostrando uma mensagem na tela
+            Console.WriteLine("Precione qualquer tecla para continuar...");
+            //Espera que alguma tecla seja precionada para encerrar a aplicação
+            Console.ReadKey();
+        }
+    }
+}
